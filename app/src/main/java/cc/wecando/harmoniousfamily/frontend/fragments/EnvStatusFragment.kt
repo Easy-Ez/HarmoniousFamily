4fa11700d10ca97f07699dc79ac8b6bb0bddafcf
sadhu <static.sadhu@gmail.com>
2021-12-16 22:54:51 +0800

    status fragment 状态更新

--- app/src/main/java/cc/wecando/harmoniousfamily/frontend/fragments/EnvStatusFragment.kt
+++ app/src/main/java/cc/wecando/harmoniousfamily/frontend/fragments/EnvStatusFragment.kt
@@ -4,10 +4,14 @@ import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import cc.wecando.harmoniousfamily.Global.ACTION_REQUIRE_HOOK_STATUS
 import cc.wecando.harmoniousfamily.R
@@ -33,8 +37,59 @@ class EnvStatusFragment : Fragment() {
         return bind.root
     }
 
-    fun isModuleLoaded() = false
-    fun getXposedVersion() = 0
+    override fun onStart() {
+        super.onStart()
+        if (isModuleLoaded()) {
+            // Set the main banner of status fragment.
+            val color: Int
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N || getXposedVersion() >= 89) {
+                color = ContextCompat.getColor(requireActivity(), R.color.ok)
+                bind.statusText.text = getString(R.string.status_ok)
+                bind.statusImage.setImageResource(R.drawable.ic_status_ok)
+                bind.statusImage.contentDescription = getString(R.string.status_ok)
+            } else {
+                color = ContextCompat.getColor(requireActivity(), R.color.warn)
+                bind.statusText.text = getString(R.string.status_outdated_xposed)
+                bind.statusImage.setImageResource(R.drawable.ic_status_error)
+                bind.statusImage.contentDescription = getString(R.string.status_outdated_xposed)
+            }
+            bind.statusText.setTextColor(color)
+            bind.statusImage.setBackgroundColor(color)
+
+            // Set the status for each component.
+            requireHookStatus(requireActivity()) { status ->
+                for (entry in componentMap) {
+                    if (status.contains(entry.key)) {
+                        setComponentIconValid(entry.value)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setComponentIconValid(iconId: Int) {
+        val icon = bind.root.findViewById<ImageView>(iconId)
+        if (icon != null) {
+            icon.setImageResource(R.drawable.ic_component_valid)
+            icon.contentDescription = getString(R.string.status_component_valid)
+        }
+    }
+
+    // Check backend.WechatHook for actual implementation
+    private fun isModuleLoaded(): Boolean {
+        // In some frameworks, short methods (less than two Dalvik instructions)
+        // can not be hooked stably. This log just makes the method longer to hook.
+        Log.v(TAG, "$javaClass.isModuleLoaded() invoked.")
+        return false
+    }
+
+    // Check backend.WechatHook for actual implementation
+    private fun getXposedVersion(): Int {
+        // In some frameworks, short methods (less than two Dalvik instructions)
+        // can not be hooked stably. This log just makes the method longer to hook.
+        Log.v(TAG, "$javaClass.getXposedVersion() invoked. ")
+        return 0
+    }
 
     companion object {
         private const val TAG = "StatusFragment"
