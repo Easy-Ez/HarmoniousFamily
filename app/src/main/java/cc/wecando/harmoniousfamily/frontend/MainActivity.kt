fa08ddc3c119a31a4c2cccafd819fe76c37a8142
sadhu <static.sadhu@gmail.com>
2021-12-27 23:36:16 +0800

    避免每次重复创建 fragment

--- app/src/main/java/cc/wecando/harmoniousfamily/frontend/MainActivity.kt
+++ app/src/main/java/cc/wecando/harmoniousfamily/frontend/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.FragmentTransaction
 import cc.wecando.harmoniousfamily.Global.PREFERENCE_NAME_DEVELOPER
 import cc.wecando.harmoniousfamily.Global.PREFERENCE_NAME_SETTINGS
 import cc.wecando.harmoniousfamily.R
@@ -56,7 +57,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.main_container, EnvStatusFragment.newInstance())
+                .add(
+                    R.id.main_container,
+                    EnvStatusFragment.newInstance(),
+                    R.id.nav_status.toString()
+                )
                 .commit()
         }
     }
@@ -70,25 +75,43 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        val fragment = when (item.itemId) {
-            R.id.nav_status -> {
-                EnvStatusFragment.newInstance()
+        val cacheFragment = supportFragmentManager.findFragmentByTag(item.itemId.toString())
+        val transaction = supportFragmentManager.beginTransaction()
+        if (cacheFragment == null) {
+            val fragment = when (item.itemId) {
+                R.id.nav_status -> {
+                    EnvStatusFragment.newInstance()
+                }
+                R.id.nav_settings -> {
+                    PrefFragment.newInstance(R.xml.pref_settings, PREFERENCE_NAME_SETTINGS)
+                }
+                R.id.nav_developer -> {
+                    PrefFragment.newInstance(R.xml.pref_developer, PREFERENCE_NAME_DEVELOPER)
+                }
+                else ->
+                    throw Error("Unknown navigation item: ${item.itemId}")
             }
-            R.id.nav_settings -> {
-                PrefFragment.newInstance(R.xml.pref_settings, PREFERENCE_NAME_SETTINGS)
-            }
-            R.id.nav_developer -> {
-                PrefFragment.newInstance(R.xml.pref_developer, PREFERENCE_NAME_DEVELOPER)
-            }
-            else ->
-                throw Error("Unknown navigation item: ${item.itemId}")
+            transaction.add(R.id.main_container, fragment, item.itemId.toString())
         }
+        toggleFragment(item, transaction)
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, fragment)
-            .commit()
 
         bind.drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
+
+
+    private fun toggleFragment(item: MenuItem, beginTransaction: FragmentTransaction) {
+        for (frag in supportFragmentManager.fragments) {
+            if (frag.isAdded) {
+                if (frag.tag == item.itemId.toString()) {
+                    beginTransaction.show(frag)
+                } else {
+                    beginTransaction.hide(frag)
+                }
+            }
+        }
+        beginTransaction.commitAllowingStateLoss()
+    }
+
 }
